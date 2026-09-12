5deb0f41dc31ead3a5ed82058fc6760d33aa6b86
Mipjeiger <dianhinhin123@gmail.com>
2026-09-13 00:05:42 +0800

    deleted which are doesn't needed and add config

---
+++ credit_risk_production/app/api/config.py
@@ -0,0 +1,11 @@
+import os
+from pathlib import Path
+
+class Settings:
+    MLFLOW_TRACKING_URI: str = os.getenv("MLFLOW_TRACKING_URI", "http://mlflow:5000")
+    MLFLOW_MODEL_NAME: str = os.getenv("MLFLOW_MODEL_NAME", "credit_risk_bundle")
+    MLFLOW_STAGE: str = os.getenv("MLFLOW_STAGE", "Production")
+    RAG_BASE_DIR: str = os.getenv("RAG_BASE_DIR", "./database/LLM")
+    LOG_LEVEL: str = os.getenv("LOG_LEVEL", "INFO")
+
+settings = Settings()
